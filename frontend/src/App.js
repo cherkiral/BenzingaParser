37e8207b3a8777aa5067ed4ccfc5132dc29e15b9
kirill <cherkiral@gmail.com>
2023-12-03 17:39:17 +0300

    added readme

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import ArticleFetcher from './ArticleFetcher';
+
+function App() {
+  return (
+      <div className="App">
+        <header className="App-header">
+          <ArticleFetcher />
+        </header>
+      </div>
+  );
+}
+
+export default App;
